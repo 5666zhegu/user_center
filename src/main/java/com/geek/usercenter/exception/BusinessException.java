47f9fbb8b17305a93271d39545a24331eb60b1a2
Knight <liujunfeng242@gmail.com>
2026-03-14 21:41:05 +0800

    Custom Exception Code Development

---
+++ src/main/java/com/geek/usercenter/exception/BusinessException.java
@@ -0,0 +1,34 @@
+package com.geek.usercenter.exception;
+
+import com.geek.usercenter.common.ErrorCode;
+
+/**
+ * 自定义异常类
+ */
+public class BusinessException extends RuntimeException{
+
+    private int code;
+
+    private String description;
+
+    public BusinessException(int code, String message, String description){
+        super(message);
+        this.code = code;
+        this.description = description;
+    }
+
+    public BusinessException(ErrorCode errorCode, String description){
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+}
