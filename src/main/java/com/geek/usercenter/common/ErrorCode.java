83dafb456fb8d2885ea7401949d22b6e80410db4
Knight <liujunfeng242@gmail.com>
2026-03-23 20:10:26 +0800

    Collect details

--- src/main/java/com/geek/usercenter/common/ErrorCode.java
+++ src/main/java/com/geek/usercenter/common/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     NULL_ERROR(40001,"传入数据为空",""),
     NOT_LOGIN(40100,"未登录",""),
     NO_AUTH(40101,"无权限",""),
-    SYSTEM_ERROR(50000,"系统内部异常","");
+    SYSTEM_ERROR(50000,"系统内部异常",""),
+    STATUS_ERROR(40002,"状态异常","");
+
 
 
 
--- src/main/java/com/geek/usercenter/common/ResultUtils.java
+++ src/main/java/com/geek/usercenter/common/ResultUtils.java
@@ -3,22 +3,22 @@ package com.geek.usercenter.common;
 public class ResultUtils {
 
     public static <T> BaseResponse<T> success(T data){
-        return new BaseResponse<>(1, data,"ok","Code run success");
+        return new BaseResponse<>(0, data,"ok","Code run success");
     }
 
-    public static BaseResponse success(){
-        return new BaseResponse(1,"ok","Code run success");
+    public static <T> BaseResponse<T> success(){
+        return new BaseResponse<>(0,"ok","Code run success");
     }
 
-    public static BaseResponse error(ErrorCode errorCode,String description){
-        return new BaseResponse(errorCode,description);
+    public static <T> BaseResponse<T> error(ErrorCode errorCode,String description){
+        return new BaseResponse<>(errorCode,description);
     }
 
-    public static BaseResponse error(ErrorCode errorCode,String message, String description){
-        return new BaseResponse(errorCode.getCode(),message,description);
+    public static <T> BaseResponse<T> error(ErrorCode errorCode,String message, String description){
+        return new BaseResponse<>(errorCode.getCode(),message,description);
     }
 
-    public static BaseResponse error(int code,String message,String description ){
-        return new BaseResponse(code,message,description);
+    public static <T> BaseResponse<T> error(int code,String message,String description ){
+        return new BaseResponse<>(code,message,description);
     }
 }
